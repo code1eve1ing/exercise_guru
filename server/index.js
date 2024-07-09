a0a2e517c80a21f707e6c7964292fb2184d3b705
Sanket Mistry <sanket.mern.dev@gmail.com>
2024-07-09 07:29:21 +0530

    Debug code

--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.mongo_url)
-    .then(()=>console.log('mongodb connected'))
-    .catch(err=>console.log(err))
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err))
+
+app.use((req, res, next) => {
+    console.log('Request Headers:', req.headers);
+    next();
+});
 
 // TODO: add cors options 
 app.use(cors())
@@ -16,7 +21,7 @@ app.options('*', cors())
 // Allows to send json data from client to server
 app.use(express.json())
 
-app.listen(port,'localhost', () => {
+app.listen(port, 'localhost', () => {
     console.log('Server started on port :', port)
 })
 
