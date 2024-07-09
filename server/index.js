2230d0427cc6aa0676ee2dbcc1cd8b4b8de72cd7
Sanket Mistry <sanket.mern.dev@gmail.com>
2024-07-09 07:24:11 +0530

    Update cors options

--- server/index.js
+++ server/index.js
@@ -5,18 +5,13 @@ const app = express()
 const configRoute = require('./util/global/configRoute')
 require('dotenv').config();
 const mongoose = require('mongoose')
-console.log('Client origin :', process.env.client)
-const corsOptions = {
-    origin: process.env.client,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']
-}
 
 mongoose.connect(process.env.mongo_url)
     .then(()=>console.log('mongodb connected'))
     .catch(err=>console.log(err))
 
-app.use(cors(corsOptions))
+// TODO: add cors options 
+app.use(cors())
 app.options('*', cors(corsOptions))
 // Allows to send json data from client to server
 app.use(express.json())
