e22e57a2be9e53a643527d20eae1840fdb99bcb7
Sanket Mistry <sanket.mern.dev@gmail.com>
2024-07-09 08:05:50 +0530

    Update cors options

--- server/index.js
+++ server/index.js
@@ -13,11 +13,13 @@ mongoose.connect(process.env.mongo_url)
 
 // Allows to send json data from client to server
 app.use(express.json())
-app.use(cors({
+
+const corsOptions = {
     origin: 'https://exercise-guru.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     optionsSuccessStatus: 204
-}));
+}
+app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.listen(port, () => {
     console.log('Server started on port :', port)
